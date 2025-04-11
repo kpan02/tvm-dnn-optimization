268b2d169c978981958d8c5fa624f9609a1ad07d
Kevin Pan <89890227+kpan02@users.noreply.github.com>
2025-04-11 00:54:29 -0400

    conv2d_gpu optimizations

--- src/ops.py
+++ src/ops.py
@@ -43,10 +43,12 @@ def conv1d_cpu_optim1(s, B):
     s[B].reorder(io, ii)
     return s, ii, io  
 
+
 def conv1d_cpu_optim2(s, B, inner_axis):
     # Optimization 2: Vectorization
     s[B].vectorize(inner_axis)
     return s
+
 
 def conv1d_cpu_optim3(s, B, outer_axis):
     # Optimization 3: Parallel execution
@@ -95,10 +97,12 @@ def conv1d_gpu_optim1(s, B):
     s[B].bind(tx, te.thread_axis("threadIdx.x"))
     return s, bx, tx
 
+
 def conv1d_gpu_optim2(s, B, bx, tx, k):
     # Optimization 2: Reorder axes
     s[B].reorder(bx, tx, k)
     return s
+
 
 def conv1d_gpu_optim3(s, B, tx):
    # Optimization 3: Auto-unroll pragma
@@ -206,10 +210,113 @@ def make_dwsp_conv2d_gpu_scheduler(B, C, H, W, K):
     assert K % 2 == 1
     inp = te.placeholder((B, C, H, W), name="A")
     ker = te.placeholder((C, 1, K, K), name="W")
-
-    # TODO: fill-in start
-    out = None
-    s = None
-    # TODO: fill-in end
-
+    pad = (K - 1) // 2
+    
+    padded = te.compute(
+        (B, C, H + 2*pad, W + 2*pad),
+        lambda b, c, h, w: tvm.tir.if_then_else(
+            tvm.tir.all(h >= pad, h < H + pad, w >= pad, w < W + pad),
+            inp[b, c, h - pad, w - pad],
+            tvm.tir.const(0.0, "float32")
+        ),
+        name="padded"
+    )
+    
+    r_h = te.reduce_axis((0, K), name="r_h")
+    r_w = te.reduce_axis((0, K), name="r_w")
+    
+    # Depthwise convolution 
+    out = te.compute(
+        (B, C, H, W),
+        lambda b, c, h, w: te.sum(
+            padded[b, c, h + r_h, w + r_w] * ker[c, 0, r_h, r_w],
+            axis=[r_h, r_w]
+        ),
+        name="out"
+    )
+    
+    # Create schedule
+    s = te.create_schedule(out.op)
+    
+    # Optimization 1: Tiling and Thread Binding
+    s, block_axis = dwsp_conv2d_gpu_optim1(s, out, inp, ker, padded)
+    
+    # Optimization 2: Vectorization and Unrolling
+    s = dwsp_conv2d_gpu_optim2(s, out, inp, ker, padded, block_axis)
+    
+    # Optimization 3: Loop reordering
+    s = dwsp_conv2d_gpu_optim3(s, out, inp, ker, padded, block_axis)
+    
     return s, inp, ker, out
+
+
+def dwsp_conv2d_gpu_optim1(s, out, inp, ker, padded):
+    # Optimization 1: Tiling and Thread Binding
+    
+    b, c, h, w = s[out].op.axis
+    r_h, r_w = s[out].op.reduce_axis
+    tile_size = 16
+    
+    b_outer, b_inner = s[out].split(b, factor=1)
+    c_outer, c_inner = s[out].split(c, factor=4)
+    h_outer, h_inner = s[out].split(h, factor=tile_size)
+    w_outer, w_inner = s[out].split(w, factor=tile_size)
+    s[out].reorder(b_outer, c_outer, h_outer, w_outer, b_inner, c_inner, h_inner, w_inner, r_h, r_w)
+    
+    # Bind to thread hierarchy
+    s[out].bind(b_outer, te.thread_axis("blockIdx.z"))
+    s[out].bind(c_outer, te.thread_axis("blockIdx.y"))
+    fused_hw = s[out].fuse(h_outer, w_outer)
+    s[out].bind(fused_hw, te.thread_axis("blockIdx.x"))
+    
+    fused_c_inner = s[out].fuse(b_inner, c_inner)
+    s[out].bind(fused_c_inner, te.thread_axis("threadIdx.z"))
+    s[out].bind(h_inner, te.thread_axis("threadIdx.y"))
+    s[out].bind(w_inner, te.thread_axis("threadIdx.x"))
+    
+    return s, fused_hw
+
+
+def dwsp_conv2d_gpu_optim2(s, out, inp, ker, padded, block_axis):
+    # Optimization 2: Vectorization and Unrolling
+
+    s[padded].compute_inline()
+    thread_axes = []
+    for ax in s[out].leaf_iter_vars:
+        if ax.var.name == 'threadIdx.x' or ax.var.name == 'threadIdx.y':
+            thread_axes.append(ax)
+    
+    if len(thread_axes) >= 1:
+        s[out].pragma(thread_axes[0], "auto_unroll_max_step", 512)
+        s[out].pragma(thread_axes[0], "unroll_explicit", True)
+    
+    return s
+
+
+def dwsp_conv2d_gpu_optim3(s, out, inp, ker, padded, block_axis):
+    # Optimization 3: Loop reordering for reduction axes
+    reduce_axes = []
+    for ax in s[out].op.reduce_axis:
+        reduce_axes.append(ax)
+
+    spatial_axes = []
+    for ax in s[out].leaf_iter_vars:
+        if ax.var.name.startswith('thread'):
+            spatial_axes.append(ax)
+
+    if reduce_axes and spatial_axes:
+        current_order = list(s[out].leaf_iter_vars)
+        new_order = []
+        for ax in current_order:
+            if ax not in reduce_axes:
+                new_order.append(ax)
+
+        if len(reduce_axes) >= 2:
+            r_h, r_w = reduce_axes
+            new_order.append(r_w)  # Putting r_w before r_h
+            new_order.append(r_h)
+        else:
+            new_order.extend(reduce_axes)
+
+        s[out].reorder(*new_order)
+    return s
